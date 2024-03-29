c7c90737b8eb24bb8e7fa08afe1bb8949a977a15
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-29 12:48:27 +0000

    connect and consume order and auth email

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,12 @@ import http from 'http';
 import { Application } from 'express';
 import { config } from '@notifications/config';
 import { Logger } from 'winston';
+import { Channel } from 'amqplib';
 import { winstonLogger } from '@wrightkhlebisol/jobber-shared';
 import { healthRoutes } from '@notifications/routes';
+import { checkConnection } from '@notifications/elasticsearch';
+import { createConnectionAndChannel } from '@notifications/queues/connection';
+import { consumeAuthEmailNotification, consumeOrderEmailNotification } from '@notifications/queues/email.consumer';
 
 const SERVER_PORT = 4001;
 
@@ -23,7 +27,6 @@ function startServer(app: Application): void {
     const httpServer: http.Server = new http.Server(app);
     httpServer.listen(SERVER_PORT, () => {
       log.info(`Notification server running on port ${SERVER_PORT}`);
-
     });
     log.info(`Worker with process id of ${process.pid} for notification service started`);
   } catch (error) {
@@ -32,9 +35,19 @@ function startServer(app: Application): void {
 }
 
 async function startQueues(): Promise<void> {
+  const emailChannel: Channel = await createConnectionAndChannel() as Channel;
+  await consumeAuthEmailNotification(emailChannel);
+  await consumeOrderEmailNotification(emailChannel);
+  await emailChannel.assertExchange('jobber-auth-notification', 'direct', { durable: false });
+  const message = JSON.stringify({ name: 'jobber-auth', service: 'notification service - auth' });
+  emailChannel.publish('jobber-auth-notification', 'auth-email', Buffer.from(message));
+
+  await emailChannel.assertExchange('jobber-order-notification', 'direct', { durable: false });
+  const message1 = JSON.stringify({ name: 'jobber-order', service: 'notification service - order' });
+  emailChannel.publish('jobber-auth-notification', 'auth-email', Buffer.from(message1));
 
 }
 
 function startElasticSearch(): void {
-
+  checkConnection();
 }
