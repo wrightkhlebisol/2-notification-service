d1326ae454b56229c3beb9770675e892c33f7260
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-30 17:28:10 +0000

    implement helper for sending templated emails

---
+++ src/helpers.ts
@@ -0,0 +1,56 @@
+import path from 'path';
+
+import { config } from '@notifications/config';
+import Email from 'email-templates';
+import { Logger } from 'winston';
+import nodemailer, { Transporter } from 'nodemailer';
+import { winstonLogger, IEmailLocals } from '@wrightkhlebisol/jobber-shared';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'mail-transport-helpers', 'debug');
+
+export async function sendEmailWithTemplates(template: string, receiver: string, locals: IEmailLocals): Promise<void> {
+  try {
+    const transporter: Transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: config.EMAIL_SENDER,
+        pass: config.EMAIL_SENDER_PASSWORD,
+      }
+    });
+
+    const email: Email = new Email({
+      message: {
+        from: `Jobber App <${config.EMAIL_SENDER}>`
+      },
+      send: true,
+      preview: false,
+      transport: transporter,
+      views: {
+        options: {
+          extension: 'ejs'
+        }
+      },
+      juice: true,
+      juiceResources: {
+        preserveImportant: true,
+        webResources: {
+          relativeTo: path.join(__dirname, '../build'),
+          images: true
+        }
+      }
+    });
+
+    await email.send({
+      template,
+      message: {
+        to: receiver
+      },
+      locals
+    });
+
+    log.info(`Sending email to ${receiver} with template ${template} and data ${JSON.stringify(locals)}`);
+  } catch (error) {
+    log.error('NotificationService sendEmail() method error', error);
+  }
+}
