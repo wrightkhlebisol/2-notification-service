eb0ea2dbde820d39d33959a8fc07e72b4b102ef4
wrightkhlebisol <calebogundiya@gmail.com>
2024-04-01 01:37:23 +0100

    transport for sending emails

---
+++ src/queues/mail.transport.ts
@@ -0,0 +1,15 @@
+import { config } from '@notifications/config';
+import { IEmailLocals, winstonLogger } from '@wrightkhlebisol/jobber-shared';
+import { Logger } from 'winston';
+import { sendEmailWithTemplates } from '@notifications/helpers';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'email-transport', 'debug');
+
+export async function sendEmail(template: string, recipient: string, locals: IEmailLocals): Promise<void> {
+  try {
+    sendEmailWithTemplates(template, recipient, locals);
+    log.info(`Email to ${recipient} sent successfully.`);
+  } catch (error) {
+    log.error('NotificationService sendEmail() method error', error);
+  }
+}
